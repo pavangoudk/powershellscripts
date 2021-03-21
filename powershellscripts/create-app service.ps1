5dbcb0a66b260a8de9ca0b2b32bc39f72aae90e4
pavan <pavan.kgoud@gmail.com>
2021-03-21 19:33:47 +0530

    ps of app service

---
+++ powershellscripts/create-app service.ps1
@@ -0,0 +1,22 @@
+﻿#creating appservices
+
+#Connect-AzAccount
+
+#Login-AzAccount
+
+$resourcegroup = "htmlwebsitedeployment"
+$location = "central us"
+$appservice = "htmappserviceplan"
+$webapp = "htmlwebappcss001"
+
+#create new resource group
+New-AzResourceGroup -Name $resourcegroup -Location $location
+
+#create webservice app plan 
+New-AzAppServicePlan -Name $appservice -Location $location -ResourceGroupName $resourcegroup -Tier Standard
+
+#create webapp 
+New-AzWebApp  -name $webapp -ResourceGroupName $resourcegroup -Location $location -AppServicePlan $appservice
+
+#creation of applicationinsights
+New-AzApplicationInsights -Name "intelpeace" -ResourceGroupName "disneyworld" -Location 'East Asia' 
