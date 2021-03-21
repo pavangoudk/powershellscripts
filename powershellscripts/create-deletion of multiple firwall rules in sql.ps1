a80bec587027cb30a0798c00b1273ed57baf6491
pavan <pavan.kgoud@gmail.com>
2021-03-21 19:38:42 +0530

    firewall in sql

---
+++ powershellscripts/create-deletion of multiple firwall rules in sql.ps1
@@ -0,0 +1,13 @@
+﻿#login to azure
+Connect-AzAccount
+
+#get all firewall rules with same rule name
+$firestore = Get-AzSqlServerFirewallRule -ServerName "wildcraft" -ResourceGroupName "orderstatus" | Where-Object {$_.FirewallRuleName -like "client*"}
+
+#storing the firewall rules in array
+$abc=@($firestore)
+
+#looping them using foreach
+foreach($i in $abc){
+Remove-AzSqlServerFirewallRule -ServerName wildcraft -ResourceGroupName "orderstatus" -FirewallRuleName $i.FirewallRuleName
+}
