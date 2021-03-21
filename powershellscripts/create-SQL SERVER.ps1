5b7f1381dd7cbc47c39b1fbc6cccc70c997a8f85
pavan <pavan.kgoud@gmail.com>
2021-03-21 19:01:44 +0530

    sql server creation

---
+++ powershellscripts/create-SQL SERVER.ps1
@@ -0,0 +1,26 @@
+﻿#SQL SERVER creation script
+
+
+#this will create us username and password  to login for that session
+$logindetails = Get-Credential 
+
+
+#creation of sever
+New-AzSqlServer -resourcegroupname 'rg-mgcorp-dev' -ServerName 'sql-mgcorp-dev' -Location 'northeurope' -SqlAdministratorCredentials $logindetails 
+
+
+#method 2
+#creating username and password anad assigning to the server
+
+$userName = 'serveruser'
+$password = get-content "C:\Users\pavan\Desktop\password.txt"
+
+$pwdSecureString = ConvertTo-SecureString -Force -AsPlainText $password
+
+
+$credential = New-Object -TypeName System.Management.Automation.PSCredential `
+              -ArgumentList $userName, $pwdSecureString
+
+
+#creation of sql server
+New-AzSqlServer -resourcegroupname "orderstatus" -ServerName "sql-" -Location northeurope -SqlAdministratorCredentials $logindetails  
