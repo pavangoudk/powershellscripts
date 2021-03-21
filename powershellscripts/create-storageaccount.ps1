413332ff4667adf0d77667af6f25a736b4dcad10
pavan <pavan.kgoud@gmail.com>
2021-03-21 12:46:13 +0530

    ps regarding azurestorage

---
+++ powershellscripts/create-storageaccount.ps1
@@ -0,0 +1,48 @@
+﻿#first method to create storage account
+
+param($rname,$sname,$location)
+
+$rname = read-host "enter the resourcename"
+$sname = read-host "enter the storageaccount"
+$location = read-host "enter the location"
+
+#creating a resourcegroup
+New-AzResourceGroup -name $rname -location $location
+
+#creating a storageaccount
+New-AzStorageAccount -resourcegroup $rname -name $sname -Location $location -SkuName Standard_LRS -Kind StorageV2
+
+
+
+
+
+
+
+#second method to create storage account
+
+
+#creating a resourcegroup
+New-AzResourceGroup -name 'rg-mgcorp-dev' -Location 'east us'
+
+#creating a storageaccount
+new-azstorageaccount -ResourceGroup 'rg-mgcorp-dev' -location 'east us' -SkuName Standard_LRS -Kind StorageV2 -Tag @{"environment"="dev";"project"="automobiles"}
+
+
+
+
+
+
+
+#third method to create storage account
+
+$rname = 'rg-mgcorp-dev'
+$sname = "mgcorpdev"
+$location = "eastus"
+
+
+#creating a resourcegroup
+New-AzResourceGroup -Name $rname -Location $location
+
+
+#creating a storageaccount
+New-AzStorageAccount -ResourceGroupName $rname -Name $sname -Location $location -SkuName Standard_ZRS -Kind StorageV2 -Tag @{"environment"="dev";"project"="automobiles"}
---
+++ powershellscripts/create-storagecontainer.ps1
@@ -0,0 +1,26 @@
+﻿#creating storagecontainer
+
+$resourceGroup = "rg-mgcorp-dev"
+$storageAccount = "mgcorpdev"
+$container = "mgcorpcontainer"
+$location = "north europe"
+
+
+# Log in to Azure
+Connect-AzAccount
+
+
+# Create your Azure resource group
+New-AzResourceGroup -Name $resourceGroup -Location $location
+
+# Create your Azure storage account
+
+$account = New-AzStorageAccount -ResourceGroupName $resourceGroup -StorageAccountName $storageAccount `
+-SkuName Standard_ZRS -Location $location -Kind StorageV2
+
+# Create a new container using the context
+$container = New-AzStorageContainer -Name $container -Context $account.Context
+
+#To logout from azure
+Logout-AzAccount
+
